73ae5a048078d732db5998aa86479961a90193cc
leon <l.sottru@web.de>
2023-09-26 15:23:11 +0200

    ann createString method

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,8 +10,27 @@ def hello_world():
 
 @app.route("/api/hello", methods=["GET"])
 def hello_world2():
-    return "Hello, Hello!"
+    return render_template("index.html")
+
+
+@app.route("/api/todo", methods=["GET"])
+def todo():
+    mystring = createString()
+    return mystring
 
 
 if __name__ == "__main__":
     app.run(port=5328)
+
+
+def createString():
+    return "createdString"
+
+
+class Todo:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return f"{self.name} : {self.description}"
