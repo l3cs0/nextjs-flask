9f1e6700323473526741410332c6b8ba17677848
leon <l.sottru@web.de>
2023-09-26 16:17:07 +0200

    add solver

--- api/capacityService/solver.py
+++ api/capacityService/solver.py
@@ -12,3 +12,23 @@ def solve():
     y = solver.NumVar(0, 2, "y")
 
     print("Number of variables =", solver.NumVariables())
+
+    # Create a linear constraint, 0 <= x + y <= 2.
+    ct = solver.Constraint(0, 2, "ct")
+    ct.SetCoefficient(x, 1)
+    ct.SetCoefficient(y, 1)
+
+    print("Number of constraints =", solver.NumConstraints())
+
+    # Create the objective function, 3 * x + y.
+    objective = solver.Objective()
+    objective.SetCoefficient(x, 3)
+    objective.SetCoefficient(y, 1)
+    objective.SetMaximization()
+
+    solver.Solve()
+    print("Solution:")
+    print("Objective value =", objective.Value())
+    print("x =", x.solution_value())
+    print("y =", y.solution_value())
+    return "solved"
