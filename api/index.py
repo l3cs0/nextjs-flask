3c8c683b2f009d519ad9d7d0446490392db0b390
leon <l.sottru@web.de>
2023-09-26 10:54:14 +0200

    add second route, working version

--- api/index.py
+++ api/index.py
@@ -1,6 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/api/python")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, Python!</p>"
+
+
+@app.route("/api/hello", methods=["GET"])
+def hello_world2():
+    return "Hello, Hello!"
