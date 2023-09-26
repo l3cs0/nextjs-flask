66470d0284f6720eaab5866c89a848e895696dba
leon <l.sottru@web.de>
2023-09-26 16:33:58 +0200

    Revert "use solver"
    This reverts commit fc1eecce62c71bcf36ba150a22bdcc15dfa71e58.

--- api/index.py
+++ api/index.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template
 
-from api.capacityService.solver import solve
-
 app = Flask(__name__)
 
 
@@ -18,12 +16,6 @@ def hello_world2():
 @app.route("/api/create", methods=["GET"])
 def todo():
     mystring = create_string()
-    return mystring
-
-
-@app.route("/api/solve", methods=["GET"])
-def todo():
-    mystring = solve()
     return mystring
 
 
